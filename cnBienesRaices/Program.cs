2c9e9a51c0f0e91e04e4dcfc646861395ae241ba
ManuelaCg <manuela.castano585@pascualbravo.edu.co>
2025-02-25 18:48:44 -0500

    Agregar archivos de proyecto.

---
+++ cnBienesRaices/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnBienesRaices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Console.WriteLine("Calculo de pagos para casas de interes social\n");
+
+				Console.WriteLine("Digite los ingresos del comprador: ");
+				decimal ingresos = decimal.Parse(Console.ReadLine());
+
+				Console.WriteLine("Ingrese el costo de la casa: ");
+				decimal costocasa = decimal.Parse(Console.ReadLine());
+
+				Console.WriteLine("¿Es cabeza de familia? (S/N): ");
+				bool esCabeza = Console.ReadLine().Trim().ToUpper() == "S";
+
+				clsCuotaCasa calculadora = new clsCuotaCasa(ingresos, costocasa, esCabeza);
+
+				Console.WriteLine("\nResultados: ");
+				Console.WriteLine($"Enganche requerido: {calculadora.CalcularEnganche():C}");
+				Console.WriteLine($"Cuota mensual: {calculadora.CalcularCuotaMensual():C}");
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine("Error en la entrada de datos: " + ex.Message);
+			}
+        }
+    }
+}
---
+++ cnBienesRaices/clsCuotaCasa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnBienesRaices
+{
+    public class clsCuotaCasa
+    {
+        public decimal Ingresos { get; set; }
+        public decimal CostoCasa { get; set; }
+        public bool EsCabezaFamilia { get; set; }
+
+        public clsCuotaCasa(decimal ingresos, decimal costocasa, bool escabezahogar) 
+        {
+            Ingresos = ingresos;
+            CostoCasa = costocasa;
+            EsCabezaFamilia = escabezahogar;
+        }
+
+
+        public decimal CalcularEnganche()
+        {
+            if (Ingresos < 600000m)
+                return CostoCasa * 0.15m;
+
+            return CostoCasa * 0.30m;
+        }
+
+        public decimal CalcularCuotaMensual()
+        {
+            decimal enganche = CalcularEnganche();
+            decimal resto = CostoCasa - enganche;
+
+            int años = Ingresos < 600000m ? 10 : 7;
+            int meses = años * 12;
+
+            decimal cuota = resto / meses;
+
+            if (EsCabezaFamilia)
+                cuota *= 0.97m;
+
+            return cuota;
+        }
+    }
+}
